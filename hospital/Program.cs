e0de89861c434a611b5c9eae94f53ea0b954146c
David Kaufmann <david@kaufmann.dev>
2021-01-19 09:25:24 +0100

    feat: add .gitignore

---
+++ hospital/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
